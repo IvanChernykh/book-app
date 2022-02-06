4a4e12321a93f10988dbc24236e61fd8f6dc87fa
Ivan Chernykh <ivanchernykh98@gmail.com>
2022-02-06 18:46:06 +0200

    recreated entire project

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
-import React from "react";
-import BooksResults from "./components/BooksResults";
-import Header from "./components/Header";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-const App: React.FC = () => {
+function App() {
   return (
-    <div className="app">
-      <Header />
-      <BooksResults />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
+
 export default App;
